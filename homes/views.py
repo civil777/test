218eb9f06b6c0197b52da5f14032f6054c7ea182
jake <ikdch016@gmail.com>
2020-12-05 20:27:47 +0900

    4.1

--- homes/views.py
+++ homes/views.py
@@ -4,13 +4,17 @@ from django.views import generic
 from . models import Question, Index
 from django.urls import reverse_lazy
 from . forms import NameForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+
+
 
 class IndexView(generic.ListView):
     model = Index
     template_name = 'index_list.html'
     context_object_name = 'text'
 
-class QuestionView(generic.edit.CreateView):
+class QuestionView(SuccessMessageMixin, generic.edit.CreateView):
     model = Question
     fields = [
     '이름',
@@ -19,8 +23,14 @@ class QuestionView(generic.edit.CreateView):
     '주소',
     '문의사항',]
 
+    success_message = "문의하신 글이 성공적으로 등록되었습니다!"
     success_url = reverse_lazy('homes:index')
     template_name_suffix = '_create'
+
+
+
+
+
 
 
 def get_name(request):
