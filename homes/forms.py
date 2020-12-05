d68727c4cd71be770c252898cb05f4b2d51f3330
jake <ikdch016@gmail.com>
2020-12-05 18:57:12 +0900

    4.0

---
+++ homes/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class NameForm(forms.Form):
+    이름 = forms.CharField(label='Your name', max_length=100)
+    전화번호 = forms.CharField(label='전화번호', max_length=100)
+    문의사항 = forms.CharField(label='문의사항', max_length=100)
