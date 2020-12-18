015eacb8673821de1a9a666221ae1435afc73fa5
jake <ikdch016@gmail.com>
2020-12-18 16:13:29 +0900

    user

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-@admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("username", "전화번호", "주소", "문의사항" )
 
+class CustomUserAdmin(UserAdmin):
+    model = models.User
+    list_display = ("username", "전화번호", "주소", "문의사항")
+
+admin.site.register(models.User, CustomUserAdmin)
